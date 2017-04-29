1390c640c099c4431eed91bdd42690008a524a07
Chenna_Potti <Chenna_Potti@chennakeshava>
2017-04-29 15:35:43 +0530

    1st file change

---
+++ src/venapps/Employee.java
@@ -0,0 +1,46 @@
+package venapps;
+
+public class Employee {
+	private int empId;
+	private String empName;
+	private double empSalary;
+	
+	public Employee() {
+		super();
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public double getEmpSalary() {
+		return empSalary;
+	}
+
+	public void setEmpSalary(double empSalary) {
+		this.empSalary = empSalary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName
+				+ ", empSalary=" + empSalary + "]";
+	}
+	
+	
+	
+	
+
+}
